d95d4016a68055536dc4c1dde83905681cba3ebb
Osolase120 <111635543+Osolase120@users.noreply.github.com>
2025-09-24 17:03:00 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+const form = document.getElementById('form');
+const email = document.getElementById('email');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    checkInputs();
+});
+
+function checkInputs() {
+    const emailValue = email.value.trim();
+
+    if (emailValue === '') {
+        setErrorfor(email, 'Email cannot be blank'); 
+    } else if (!isEmail(emailValue)) {
+        setErrorfor(email, 'Please enter a valid email'); 
+    } else {
+        setSuccessfor(email);
+    }
+}
+
+function setErrorfor(input, message) {
+    const formControl = input.parentElement;
+    const small = formControl.querySelector('small');
+
+    small.innerText = message;
+    formControl.className = 'form-control error';
+}
+
+// function setSuccessfor(input) {
+//     const formControl = input.parentElement;
+//     formControl.className = 'form-control success';
+// }
+
+function isEmail(email) {
+    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+}
